6d091259d82f47921ed19337a4bff0d5411ac5c7
윤다인 <a608079@naver.com>
2026-05-14 14:21:46 +0900

    docs: SpingBoot 기본 설정

---
+++ src/main/java/com/ssafy/yumyum/YumyumApplication.java
@@ -0,0 +1,12 @@
+package com.ssafy.yumyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumyumApplication.class, args);
+	}
+}
