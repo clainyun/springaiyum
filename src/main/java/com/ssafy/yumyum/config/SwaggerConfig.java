1eda66256752f7959de2075a6dbd82ce839cf75d
Hepheir <hepheir@gmail.com>
2026-05-22 16:55:34 +0900

    feat: API v1용 Swagger 설정 추가

---
+++ src/main/java/com/ssafy/yumyum/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.ssafy.yumyum.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "YumYumCoach API",
+        description = "YumYumCoach REST API 문서",
+        version = "v1"
+    )
+)
+public class SwaggerConfig {
+
+    @Bean
+    GroupedOpenApi yumyumApi() {
+        return GroupedOpenApi.builder()
+            .group("yumyum-api")
+            .pathsToMatch("/api/v1/**")
+            .build();
+    }
+}
